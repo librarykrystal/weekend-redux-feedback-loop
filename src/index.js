e1be66d638c8b9db3348dbfadd0e4acdb75574d3
Krys Frost <librarykrystal@gmail.com>
2023-02-01 23:18:57 -0600

    NPM installs: React, redux, redux-saga, react-router, logger. index.js organized.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,45 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// DONE: npm install, npm install redux, npm install react-redux, npm install redux-saga, npm install react-router-dom@5, npm install redux-logger@3
+// Added imports:
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import axios from 'axios';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+// Saga SETUP
+const sagaMiddleware = createSagaMiddleware();
+
+// REDUCERS
+
+
+
+// SAGAS
+function* watcherSaga(){
+    // yield takeEvery('FETCH_ELEMENTS', fetchElements);
+    // yield takeEvery('POST_ELEMENT', postElement);
+    console.log('watcherSaga');
+}
+
+
+// STORE
+const storeInstance = createStore(
+    combineReducers({
+        // firstReducer,
+        // secondReducer,
+        // elementListReducer,
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Running watcherSaga:
+sagaMiddleware.run(watcherSaga);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+// PROVIDER
+ReactDOM.render(<Provider store={storeInstance}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
